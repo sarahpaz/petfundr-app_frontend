d0ae6cc02a7609f59baac093be69564a0a815a48
Sarah Paz <sarah.paz@gmail.com>
2019-11-04 22:54:30 -0500

    move store into separate store file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 
-const users = () => [];
-
-const reducer = combineReducers({
-  users
-});
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+import App from "./App";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
