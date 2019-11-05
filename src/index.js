56a6c3ea5bc7ba8301f1fa4bd5021356af41742d
Sarah Paz <sarah.paz@gmail.com>
2019-11-04 22:15:32 -0500

    set up initial store and install redux, redux-thunk and react-redux packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const users = () => [];
+
+const reducer = combineReducers({
+  users
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
