ffe9f713f3f2d48d03e2d6ebe0dfb6f1830be9b2
Sarah Paz <sarah.paz@gmail.com>
2019-11-08 00:00:19 -0500

    set up componentDidMount to get current user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from "react-redux";
+
+import Login from "./components/Login/Login.component";
+import { getCurrentUser } from "./actions/currentUser";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getCurrentUser();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>PetFundr</h1>
+        <Login />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { getCurrentUser }
+)(App);
