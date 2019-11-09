3dfdbfe18e3bbad36b1fcf1ef855a92360fab850
Sarah Paz <sarah.paz@gmail.com>
2019-11-08 22:24:35 -0500

    set up component and import styles

---
+++ src/components/MainContainer/MainContainer.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MainContainer.styles.css";
+
+const MainContainer = () => {
+  return (
+    <div className="main-container">"INSIDE THE MAIN CONTAINER COMPONENT"</div>
+  );
+};
+
+export default MainContainer;
