6c9c44b3badfcf609c62d534c13c6cd41c3e00f1
Sarah Paz <sarah.paz@gmail.com>
2019-11-10 23:07:01 -0500

    connect to store, map through pets

--- src/components/PetListContainer/PetListContainer.component.jsx
+++ src/components/PetListContainer/PetListContainer.component.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./PetListContainer.styles.css";
 import PetCard from "../PetCard/PetCard.component";
 
-const PetListContainer = () => {
-  return (
-    <div className="petlist-container">
-      <PetCard />
-    </div>
-  );
+import { connect } from "react-redux";
+
+const PetList = props => {
+  const petCards = props.pets.map(p => <PetCard pet={p} key={p.id} />);
+
+  return <div>{petCards.length > 0 ? petCards : null}</div>;
 };
 
-export default PetListContainer;
+const mapStateToProps = state => {
+  return {
+    pets: state.pets
+  };
+};
+
+export default connect(mapStateToProps)(PetList);
