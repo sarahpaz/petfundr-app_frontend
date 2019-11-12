f9d9da453b52cd513f50bc8789349b2617fa35ea
Sarah Paz <sarah.paz@gmail.com>
2019-11-11 23:14:41 -0500

    change pet name into Link

--- src/components/PetCard/PetCard.component.jsx
+++ src/components/PetCard/PetCard.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./PetCard.styles.css";
+import { Link } from "react-router-dom";
 
 const PetCard = ({ pet }) => {
   return (
     <div className="pet-card">
-      <h3>{pet.attributes.name}</h3>
+      <Link to={`/pets/${pet.id}`}>
+        <h3>{pet.attributes.name}</h3>
+      </Link>
       <img src={pet.attributes.image} alt={pet.attributes.name} />
       <p>Age: {pet.attributes.age}</p>
       <p>Cause: {pet.attributes.cause}</p>
