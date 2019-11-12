547a0231b4ba47096a6e8f4fe504c425be4e2ebb
Sarah Paz <sarah.paz@gmail.com>
2019-11-11 19:31:45 -0500

    create updateSignupForm sync action creator

---
+++ src/actions/signupForm.js
@@ -0,0 +1,15 @@
+// syncrhonous action creators
+export const updateSignupForm = formData => {
+  return {
+    type: "UPDATE_SIGNUP_FORM",
+    formData
+  };
+};
+
+export const resetSignupForm = () => {
+  return {
+    type: "RESET_SIGNUP_FORM"
+  };
+};
+
+// asynchronous actions creators
