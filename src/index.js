8cbff52ebda502e71208ea062ec0a1b972d54c81
Sarah Paz <sarah.paz@gmail.com>
2019-11-12 22:59:47 -0500

    add withRouter for route props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 
