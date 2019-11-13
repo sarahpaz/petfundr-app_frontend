5130ba77c49c18039ff29777687ab00ab1616664
Sarah Paz <sarah.paz@gmail.com>
2019-11-13 16:13:05 -0500

    add pet info and owner username

--- src/components/PetPage/PetPage.component.jsx
+++ src/components/PetPage/PetPage.component.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./PetPage.styles.css";
 
 const PetPage = ({ pet }) => {
   return pet ? (
     <div className="pet-page">
-      <h1>Pet Page Component</h1>
-      <p>{pet.attributes.name}</p>
+      <h1>Meet {pet.attributes.name}!</h1>
+      <img src={pet.attributes.image} alt={pet.attributes.name} />
+      <p>Cause: {pet.attributes.cause}</p>
+      <p>Age: {pet.attributes.age}</p>
+      <p>Raise Goal: ${pet.attributes.goal}</p>
+      <p>Owner: {pet.attributes.owner.username}</p>
+      <p>Make a donation!</p>
     </div>
   ) : null;
 };
