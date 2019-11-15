55ee79b7a7102d486e1e0d3c7be8236a26232d1c
Sarah Paz <sarah.paz@gmail.com>
2019-11-14 22:51:17 -0500

    add age, owner, progress bar and donate button

--- src/components/PetPage/PetPage.component.jsx
+++ src/components/PetPage/PetPage.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./PetPage.styles.css";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const PetPage = ({ pet }) => {
   return pet ? (
@@ -10,7 +12,13 @@ const PetPage = ({ pet }) => {
       <p>Age: {pet.attributes.age}</p>
       <p>Raise Goal: ${pet.attributes.goal}</p>
       <p>Owner: {pet.attributes.owner.username}</p>
-      <p>Make a donation!</p>
+      <progress value="200" max={pet.attributes.goal}></progress>
+      <br />
+      <Button type="submit" variant="success">
+        <Link to="/donations/new" className="link-button">
+          Donate
+        </Link>
+      </Button>
     </div>
   ) : null;
 };
