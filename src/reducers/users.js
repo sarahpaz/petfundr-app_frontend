33239419e1570099aa0ba3b3462e84341d71adda
Sarah Paz <sarah.paz@gmail.com>
2019-11-14 22:51:47 -0500

    change CLEAR_ALL_USERS return to []

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_ALL_USERS":
       return action.users;
+    case "CLEAR_ALL_USERS":
+      return [];
     default:
       return state;
   }
