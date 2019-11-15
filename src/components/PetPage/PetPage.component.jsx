a1d96b685a277abd0b6a74a1e154777a15ac40cb
Sarah Paz <sarah.paz@gmail.com>
2019-11-15 00:49:59 -0500

    change progress bar to ProgressBar react-bootstrap component

--- src/components/PetPage/PetPage.component.jsx
+++ src/components/PetPage/PetPage.component.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import "./PetPage.styles.css";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import { Link } from "react-router-dom";
 
 const PetPage = ({ pet }) => {
   return pet ? (
     <div className="pet-page">
-      <h1>Meet {pet.attributes.name}!</h1>
-      <img src={pet.attributes.image} alt={pet.attributes.name} />
-      <p>Cause: {pet.attributes.cause}</p>
-      <p>Age: {pet.attributes.age}</p>
-      <p>Raise Goal: ${pet.attributes.goal}</p>
-      <p>Owner: {pet.attributes.owner.username}</p>
-      <progress value="200" max={pet.attributes.goal}></progress>
-      <br />
-      <Button type="submit" variant="success">
-        <Link to="/donations/new" className="link-button">
-          Donate
-        </Link>
-      </Button>
+      <h3>Meet {pet.attributes.name}!</h3>
+      <Card style={{ width: "35em", margin: "2em auto" }}>
+        <Card.Img
+          variant="top"
+          src={pet.attributes.image}
+          alt={pet.attributes.name}
+        />
+        <Card.Body>
+          <Card.Text>Cause: {pet.attributes.cause}</Card.Text>
+          <Card.Text>Age: {pet.attributes.age}</Card.Text>
+          <Card.Text>Raise Goal: ${pet.attributes.goal}</Card.Text>
+          <Card.Text>Owner: {pet.attributes.owner.username}</Card.Text>
+          <ProgressBar
+            striped
+            variant="info"
+            now={20} //TODO:update to show donation total
+            style={{ width: "50%", margin: "auto" }}
+          />
+          <Button type="submit" variant="success">
+            <Link to="/donations/new" className="link-button">
+              Donate
+            </Link>
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   ) : null;
 };
