fade3b467683154189f4fd55367f6e7246f43512
Sarah Paz <sarah.paz@gmail.com>
2019-11-15 22:08:25 -0500

    add ADD_DONATION case

--- src/reducers/donations.js
+++ src/reducers/donations.js
@@ -1,7 +1,11 @@
-export default (state = [], action) => {
+const initialState = [];
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_ALL_DONATIONS":
       return action.donations;
+    case "ADD_DONATION":
+      return state.concat(action.donation);
     default:
       return state;
   }
