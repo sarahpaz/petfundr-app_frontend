eae0000bce8e750a74a22b97aa7327b4096d4e6f
Sarah Paz <sarah.paz@gmail.com>
2019-11-23 18:30:08 -0500

    addconsole.log to test props

--- src/components/NewDonation/NewDonation.component.jsx
+++ src/components/NewDonation/NewDonation.component.jsx
@@ -4,15 +4,16 @@ import Button from "react-bootstrap/Button";
 import "./NewDonation.styles.css";
 import { updateNewDonationInfo } from "../../actions/newDonation"; // 1. import action creator
 import { createDonation } from "../../actions/donations";
-import {withRouter} from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 
 //* 3) Redux gives a prop (updateNewDonationInfo)
 const NewDonation = ({
   formData,
   updateNewDonationInfo,
   createDonation,
+  props
 }) => {
-  const { amount, message } = formData;
+  const { amount, message, pet_id } = formData;
 
   const handleOnChange = e => {
     const { name, value } = e.target;
@@ -20,15 +21,14 @@ const NewDonation = ({
   };
 
   const handleOnSubmit = e => {
-    // console.log("I'm props:", props);
-
     e.preventDefault();
     createDonation(formData);
+    console.log(props);
   };
 
   return (
     <div className="donation-container">
-			<h3>Make a Donation</h3>
+      <h3>Make a Donation</h3>
       <form onSubmit={handleOnSubmit}>
         <input
           type="number"
@@ -45,8 +45,8 @@ const NewDonation = ({
           placeholder="Message"
           value={message}
           onChange={handleOnChange}
-				></input>
-				{/*<input type="hidden" petId={props.location.state.petId} />*/}
+        ></input>
+        <input type="hidden" pet={pet_id} name="pet_id" />
         <br />
         <Button type="submit" variant="success" className="link-button">
           Donate
@@ -62,8 +62,10 @@ const mapStateToProps = state => {
   };
 };
 
-export default withRouter (connect(mapStateToProps, {
-  updateNewDonationInfo,
-  createDonation
-})(NewDonation));
+export default withRouter(
+  connect(mapStateToProps, {
+    updateNewDonationInfo,
+    createDonation
+  })(NewDonation)
+);
 //2. action creator passed through redux's connect (mapDispatchToProps / object shorthand)
