9c64c7918c75dea36e4bb45ec1fae8c9817d0fc7
Sarah Paz <sarah.paz@gmail.com>
2019-11-25 23:20:12 -0500

    add CLEAR_ALL_PETS case

--- src/reducers/pets.js
+++ src/reducers/pets.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_ALL_PETS":
       return action.pets;
+    case "CLEAR_ALL_PETS":
+      return [];
     default:
       return state;
   }
