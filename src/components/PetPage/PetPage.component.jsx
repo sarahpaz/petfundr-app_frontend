d1e4232266f57993c3e1658d1350579d2d76e7b8
Sarah Paz <sarah.paz@gmail.com>
2019-11-27 20:42:06 -0500

    change to class component

--- src/components/PetPage/PetPage.component.jsx
+++ src/components/PetPage/PetPage.component.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from "react";
 import "./PetPage.styles.css";
 import Card from "react-bootstrap/Card";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import NewDonation from "../NewDonation/NewDonation.component";
 
-const PetPage = ({ pet }) => {
-  return pet ? (
-    <div className="pet-page">
-      <h3>Meet {pet.attributes.name}!</h3>
-      <Card style={{ width: "35em", margin: "2em auto" }}>
-        <Card.Img
-          variant="top"
-          src={pet.attributes.image}
-          alt={pet.attributes.name}
-        />
-        <Card.Body>
-          <Card.Text>Cause: {pet.attributes.cause}</Card.Text>
-          <Card.Text>Age: {pet.attributes.age}</Card.Text>
-          <Card.Text>Raise Goal: ${pet.attributes.goal}</Card.Text>
-          <Card.Text>Owner: {pet.attributes.owner.username}</Card.Text>
-          <ProgressBar
-            striped
-            variant="info"
-            now={40} //TODO:update to show donation total
-            style={{ width: "50%", margin: "auto" }}
+class PetPage extends Component {
+  state = {
+    donations: 50
+  };
+
+  totalDonations = () => {
+    return this.setState({
+      donations: this.state.donations
+    });
+  };
+
+  render() {
+    const { pet } = this.props;
+    console.log();
+    return pet ? (
+      <div className="pet-page">
+        <h3>Meet {pet.attributes.name}!</h3>
+        <Card style={{ width: "35em", margin: "2em auto" }}>
+          <Card.Img
+            variant="top"
+            src={pet.attributes.image}
+            alt={pet.attributes.name}
           />
-          <NewDonation pet={pet} />
-        </Card.Body>
-      </Card>
-    </div>
-  ) : null;
-};
+          <Card.Body>
+            <Card.Text>Cause: {pet.attributes.cause}</Card.Text>
+            <Card.Text>Age: {pet.attributes.age}</Card.Text>
+            <Card.Text>Raise Goal: ${pet.attributes.goal}</Card.Text>
+            <Card.Text>Owner: {pet.attributes.owner.username}</Card.Text>
+            <ProgressBar
+              striped
+              variant="info"
+              now={this.state.donations} //TODO:update to show donation total
+              style={{ width: "50%", margin: "auto" }}
+            />
+            <NewDonation pet={pet} />
+          </Card.Body>
+        </Card>
+      </div>
+    ) : null;
+  }
+}
 
 export default PetPage;
