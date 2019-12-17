91d73847706eebb7c5ff8f86b025f53773d85f3f
Sarah Paz <sarah.paz@gmail.com>
2019-12-16 22:21:22 -0500

    change PetPage component to functional component

--- src/components/PetPage/PetPage.component.jsx
+++ src/components/PetPage/PetPage.component.jsx
@@ -1,33 +1,28 @@
-// import React from "react";
-import React, { Component } from "react";
+import React from "react";
 import "./PetPage.styles.css";
 import Card from "react-bootstrap/Card";
 import NewDonation from "../NewDonation/NewDonation.component";
 
-class PetPage extends Component {
-  render() {
-    const { pet } = this.props;
-    return pet ? (
-      <div className="pet-page">
-        <h3>Meet {pet.attributes.name}!</h3>
-        <Card className="pet-page-card">
-          <Card.Img
-            variant="top"
-            src={pet.attributes.image}
-            alt={pet.attributes.name}
-          />
-          <Card.Body>
-            <Card.Text>Cause: {pet.attributes.cause}</Card.Text>
-            <Card.Text>Age: {pet.attributes.age}</Card.Text>
-            <Card.Text>Raise Goal: ${pet.attributes.goal}</Card.Text>
-            <Card.Text>Owner: {pet.attributes.owner.username}</Card.Text>
+const PetPage = ({ pet }) => {
+  return pet ? (
+    <div className="pet-page">
+      <h3>Meet {pet.attributes.name}!</h3>
+      <Card className="pet-page-card">
+        <Card.Img
+          variant="top"
+          src={pet.attributes.image}
+          alt={pet.attributes.name}
+        />
+        <Card.Body>
+          <Card.Text>Cause: {pet.attributes.cause}</Card.Text>
+          <Card.Text>Age: {pet.attributes.age}</Card.Text>
+          <Card.Text>Raise Goal: ${pet.attributes.goal}</Card.Text>
+          <Card.Text>Owner: {pet.attributes.owner.username}</Card.Text>
 
-            <NewDonation pet={pet} />
-          </Card.Body>
-        </Card>
-      </div>
-    ) : null;
-  }
-}
-
+          <NewDonation pet={pet} />
+        </Card.Body>
+      </Card>
+    </div>
+  ) : null;
+};
 export default PetPage;
